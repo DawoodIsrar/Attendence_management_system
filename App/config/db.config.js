5ff369cb2f20bbae45b92aa4a24a4479aabaea83
Dudeisrar <85024302+DawoodIsrar@users.noreply.github.com>
2023-01-18 16:34:35 +0500

    change in server file

--- App/config/db.config.js
+++ App/config/db.config.js
@@ -6,10 +6,10 @@ module.exports = {
   DB: process.env.DB_NAME,
   dialect: process.env.DB_DIALECT,
   driver:process.env.DB_DRIVER,
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  }
+  // pool: {
+  //   max: 5,
+  //   min: 0,
+  //   acquire: 30000,
+  //   idle: 10000
+  // }
 };
